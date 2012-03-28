upmorpheus/PHP-src

commit a7639291b43b003c38f6caae3ebf524f9c1108be

Fixed bug #61347 (inconsist isset behavior of Arrayobject)

ext/spl/spl_array.c
@@ -578,49 +578,46 @@ static int spl_array_has_dimension_ex(int check_inherited, zval *object, zval *o
 	}
 	
 	switch(Z_TYPE_P(offset)) {
-	case IS_STRING:
-		if (check_empty) {
-			if (zend_symtable_find(spl_array_get_hash_table(intern, 0 TSRMLS_CC), Z_STRVAL_P(offset), Z_STRLEN_P(offset)+1, (void **) &tmp) != FAILURE) {
-				switch (check_empty) {
-					case 0:
-						return Z_TYPE_PP(tmp) != IS_NULL;
-					case 2:
-						return 1;
-					default:
-						return zend_is_true(*tmp);
+		case IS_STRING:
+			{
+				HashTable *ht = spl_array_get_hash_table(intern, 0 TSRMLS_CC);
+				if (zend_symtable_find(ht, Z_STRVAL_P(offset), Z_STRLEN_P(offset)+1, (void **) &tmp) != FAILURE) {
+					switch (check_empty) {
+						case 0:
+							return Z_TYPE_PP(tmp) != IS_NULL;
+						case 2:
+							return 1;
+						default:
+							return zend_is_true(*tmp);
+					}
 				}
+				return 0;
 			}
-			return 0;
-		} else {
-			return zend_symtable_exists(spl_array_get_hash_table(intern, 0 TSRMLS_CC), Z_STRVAL_P(offset), Z_STRLEN_P(offset)+1);
-		}
-	case IS_DOUBLE:
-	case IS_RESOURCE:
-	case IS_BOOL: 
-	case IS_LONG: 
-		if (offset->type == IS_DOUBLE) {
-			index = (long)Z_DVAL_P(offset);
-		} else {
-			index = Z_LVAL_P(offset);
-		}
-		if (check_empty) {
-			HashTable *ht = spl_array_get_hash_table(intern, 0 TSRMLS_CC);
-			if (zend_hash_index_find(ht, index, (void **)&tmp) != FAILURE) {
-				switch (check_empty) {
-					case 0:
-						return Z_TYPE_PP(tmp) != IS_NULL;
-					case 2:
-						return 1;
-					default:
-						return zend_is_true(*tmp);
+		case IS_DOUBLE:
+		case IS_RESOURCE:
+		case IS_BOOL: 
+		case IS_LONG: 
+			{
+				HashTable *ht = spl_array_get_hash_table(intern, 0 TSRMLS_CC);
+				if (offset->type == IS_DOUBLE) {
+					index = (long)Z_DVAL_P(offset);
+				} else {
+					index = Z_LVAL_P(offset);
 				}
+				if (zend_hash_index_find(ht, index, (void **)&tmp) != FAILURE) {
+					switch (check_empty) {
+						case 0:
+							return Z_TYPE_PP(tmp) != IS_NULL;
+						case 2:
+							return 1;
+						default:
+							return zend_is_true(*tmp);
+					}
+				}
+				return 0;
 			}
-			return 0;
-		} else {
-			return zend_hash_index_exists(spl_array_get_hash_table(intern, 0 TSRMLS_CC), index);
-		}
-	default:
-		zend_error(E_WARNING, "Illegal offset type");
+		default:
+			zend_error(E_WARNING, "Illegal offset type");
 	}
 	return 0;
 } /* }}} */

ext/spl/tests/bug61347.phpt
@@ -1,0 +1,40 @@
+--TEST--
+Bug #61347 (inconsist isset behavior of Arrayobject)
+--FILE--
+<?php
+$a = array('b' => NULL, 37 => NULL);
+var_dump(isset($a['b'])); //false
+
+$b = new ArrayObject($a);
+var_dump(isset($b['b'])); //false
+var_dump(isset($b[37])); //false
+var_dump(isset($b['no_exists'])); //false
+var_dump(empty($b['b'])); //true
+var_dump(empty($b[37])); //true
+
+var_dump(array_key_exists('b', $b)); //true
+var_dump($b['b']);
+
+$a = array('b' => '', 37 => false);
+$b = new ArrayObject($a);
+var_dump(isset($b['b'])); //true
+var_dump(isset($b[37])); //true
+var_dump(isset($b['no_exists'])); //false
+var_dump(empty($b['b'])); //true
+var_dump(empty($b[37])); //true
+
+
+--EXPECT--
+bool(false)
+bool(false)
+bool(false)
+bool(false)
+bool(true)
+bool(true)
+bool(true)
+NULL
+bool(true)
+bool(true)
+bool(false)
+bool(true)
+bool(true)

commit 813d7ad473014bb76f56ae48ddf4a313537d60fc

Fixed problem introduced in a763929 (fixing bug #61347).
The test added in 8c44c85 should now pass.

ext/spl/spl_array.c
@@ -636,7 +636,7 @@ SPL_METHOD(Array, offsetExists)
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z", &index) == FAILURE) {
 		return;
 	}
-	RETURN_BOOL(spl_array_has_dimension_ex(0, getThis(), index, 0 TSRMLS_CC));
+	RETURN_BOOL(spl_array_has_dimension_ex(0, getThis(), index, 2 TSRMLS_CC));
 } /* }}} */
 
 /* {{{ proto mixed ArrayObject::offsetGet(mixed $index)