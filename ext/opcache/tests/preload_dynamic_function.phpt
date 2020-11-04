upmorpheus/PHP-src

commit 68f80be9d1380de731930187250a7ed6b55ae196

Fixed run-time binding of preloaded dynamically declared function

Zend/zend_compile.c
@@ -1050,7 +1050,11 @@ ZEND_API int do_bind_function(zval *lcname) /* {{{ */
 		return FAILURE;
 	}
 	function = (zend_function*)Z_PTR_P(zv);
-	zv = zend_hash_set_bucket_key(EG(function_table), (Bucket*)zv, Z_STR_P(lcname));
+	if (UNEXPECTED(function->common.fn_flags & ZEND_ACC_PRELOADED)) {
+		zv = zend_hash_add(EG(function_table), Z_STR_P(lcname), zv);
+	} else {
+		zv = zend_hash_set_bucket_key(EG(function_table), (Bucket*)zv, Z_STR_P(lcname));
+	}
 	if (UNEXPECTED(!zv)) {
 		do_bind_function_error(Z_STR_P(lcname), &function->op_array, 0);
 		return FAILURE;

commit 3d61e8e1d9e1fb5c0716e80464d1c31dc2b72200

Fix dynamic function definition in preload script
We should use normal function renaming if the function is declared
during preloading itself, rather than afterwards.

This fixes a regression introduced by
68f80be9d1380de731930187250a7ed6b55ae196.

Zend/zend_compile.c
@@ -1050,7 +1050,8 @@ ZEND_API int do_bind_function(zval *lcname) /* {{{ */
 		return FAILURE;
 	}
 	function = (zend_function*)Z_PTR_P(zv);
-	if (UNEXPECTED(function->common.fn_flags & ZEND_ACC_PRELOADED)) {
+	if (UNEXPECTED(function->common.fn_flags & ZEND_ACC_PRELOADED)
+			&& !(CG(compiler_options) & ZEND_COMPILE_PRELOAD)) {
 		zv = zend_hash_add(EG(function_table), Z_STR_P(lcname), zv);
 	} else {
 		zv = zend_hash_set_bucket_key(EG(function_table), (Bucket*)zv, Z_STR_P(lcname));

ext/opcache/tests/preload_dynamic_function.inc
@@ -1,0 +1,4 @@
+<?php
+if (1) {
+    function f() {}
+}

ext/opcache/tests/preload_dynamic_function.phpt
@@ -1,0 +1,18 @@
+--TEST--
+Defining a dynamic function inside the preload script
+--INI--
+opcache.enable=1
+opcache.enable_cli=1
+opcache.optimization_level=-1
+opcache.preload={PWD}/preload_dynamic_function.inc
+--SKIPIF--
+<?php
+require_once('skipif.inc');
+if (PHP_OS_FAMILY == 'Windows') die('skip Preloading is not supported on Windows');
+?>
+--FILE--
+<?php
+var_dump(function_exists("f"));
+?>
+--EXPECT--
+bool(true)