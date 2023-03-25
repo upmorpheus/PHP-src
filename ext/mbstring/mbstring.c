upmorpheus/PHP-src

commit 6fc8d014dfce03121bb04b0e5ae1eea6c7e1f801

Fix phpGH-10648: add check function pointer into mbfl_encoding
Previously, mbstring used the same logic for encoding validation as for
encoding conversion.

However, there are cases where we want to use different logic for validation
and conversion. For example, if a string ends up with missing input
required by the encoding, or if a character is input that is invalid
as an encoding but can be converted, the conversion should succeed and
the validation should fail.

To achieve this, a function pointer mb_check_fn has been added to
struct mbfl_encoding to implement the logic used for validation.
Also, added implementation of validation logic for UTF-7, UTF7-IMAP,
ISO-2022-JP and JIS.

ext/mbstring/mbstring.c
@@ -4415,6 +4415,10 @@ MBSTRING_API int php_mb_check_encoding(const char *input, size_t length, const m
 	unsigned char *in = (unsigned char*)input;
 	unsigned int state = 0;
 
+	if (encoding->check != NULL) {
+		return encoding->check(in, length);
+	}
+
 	/* If the input string is not encoded in the given encoding, there is a significant chance
 	 * that this will be seen in the first bytes. Therefore, rather than converting an entire
 	 * buffer of 128 codepoints, convert and check just a few codepoints first */

UPGRADING
@@ -218,6 +218,16 @@ PHP 8.2 UPGRADE NOTES
     dba_fetch(string|array $key, $skip, $dba): string|false
     is still accepted, but it is recommended to use the new standard variant.
 
+- MBString
+  . mb_check_encoding() now checks input encoding more strictly.
+  . mb_detect_encoding() now checks input encoding more strictly
+    when strict detection is enabled.
+  . mb_convert_encoding() checks the input encoding more strictly
+    if multiple encodings are passed to from_encoding
+    and the mbstring.strict_detection INI directive is set to 1.
+    This change only affects the encoding selection,
+    not the result of the conversion.
+
 - Random
   . random_bytes() and random_int() now throw \Random\RandomException on CSPRNG failure.
     Previously a plain \Exception was thrown.

ext/mbstring/libmbfl/filters/mbfilter_7bit.c
@@ -64,7 +64,8 @@ const mbfl_encoding mbfl_encoding_7bit = {
 	&vtbl_7bit_wchar,
 	&vtbl_wchar_7bit,
 	mb_7bit_to_wchar,
-	mb_wchar_to_7bit
+	mb_wchar_to_7bit,
+	NULL
 };
 
 #define CK(statement)	do { if ((statement) < 0) return (-1); } while (0)

ext/mbstring/libmbfl/filters/mbfilter_base64.c
@@ -44,7 +44,8 @@ const mbfl_encoding mbfl_encoding_base64 = {
 	NULL,
 	NULL,
 	mb_base64_to_wchar,
-	mb_wchar_to_base64
+	mb_wchar_to_base64,
+	NULL
 };
 
 const struct mbfl_convert_vtbl vtbl_8bit_b64 = {

ext/mbstring/libmbfl/filters/mbfilter_big5.c
@@ -69,7 +69,8 @@ const mbfl_encoding mbfl_encoding_big5 = {
 	&vtbl_big5_wchar,
 	&vtbl_wchar_big5,
 	mb_big5_to_wchar,
-	mb_wchar_to_big5
+	mb_wchar_to_big5,
+	NULL
 };
 
 const mbfl_encoding mbfl_encoding_cp950 = {
@@ -82,7 +83,8 @@ const mbfl_encoding mbfl_encoding_cp950 = {
 	&vtbl_cp950_wchar,
 	&vtbl_wchar_cp950,
 	mb_cp950_to_wchar,
-	mb_wchar_to_cp950
+	mb_wchar_to_cp950,
+	NULL
 };
 
 const struct mbfl_convert_vtbl vtbl_big5_wchar = {

ext/mbstring/libmbfl/filters/mbfilter_cp5022x.c
@@ -61,7 +61,8 @@ const mbfl_encoding mbfl_encoding_cp50220 = {
 	&vtbl_cp50220_wchar,
 	&vtbl_wchar_cp50220,
 	mb_cp5022x_to_wchar,
-	mb_wchar_to_cp50220
+	mb_wchar_to_cp50220,
+	NULL
 };
 
 const mbfl_encoding mbfl_encoding_cp50221 = {
@@ -74,7 +75,8 @@ const mbfl_encoding mbfl_encoding_cp50221 = {
 	&vtbl_cp50221_wchar,
 	&vtbl_wchar_cp50221,
 	mb_cp5022x_to_wchar,
-	mb_wchar_to_cp50221
+	mb_wchar_to_cp50221,
+	NULL
 };
 
 const mbfl_encoding mbfl_encoding_cp50222 = {
@@ -87,7 +89,8 @@ const mbfl_encoding mbfl_encoding_cp50222 = {
 	&vtbl_cp50222_wchar,
 	&vtbl_wchar_cp50222,
 	mb_cp5022x_to_wchar,
-	mb_wchar_to_cp50222
+	mb_wchar_to_cp50222,
+	NULL
 };
 
 const struct mbfl_convert_vtbl vtbl_cp50220_wchar = {

ext/mbstring/libmbfl/filters/mbfilter_cp51932.c
@@ -69,7 +69,8 @@ const mbfl_encoding mbfl_encoding_cp51932 = {
 	&vtbl_cp51932_wchar,
 	&vtbl_wchar_cp51932,
 	mb_cp51932_to_wchar,
-	mb_wchar_to_cp51932
+	mb_wchar_to_cp51932,
+	NULL
 };
 
 const struct mbfl_convert_vtbl vtbl_cp51932_wchar = {

ext/mbstring/libmbfl/filters/mbfilter_cp932.c
@@ -100,7 +100,8 @@ const mbfl_encoding mbfl_encoding_cp932 = {
 	&vtbl_cp932_wchar,
 	&vtbl_wchar_cp932,
 	mb_cp932_to_wchar,
-	mb_wchar_to_cp932
+	mb_wchar_to_cp932,
+	NULL
 };
 
 const struct mbfl_convert_vtbl vtbl_cp932_wchar = {
@@ -133,7 +134,8 @@ const mbfl_encoding mbfl_encoding_sjiswin = {
 	&vtbl_sjiswin_wchar,
 	&vtbl_wchar_sjiswin,
 	mb_cp932_to_wchar,
-	mb_wchar_to_sjiswin
+	mb_wchar_to_sjiswin,
+	NULL
 };
 
 const struct mbfl_convert_vtbl vtbl_sjiswin_wchar = {

commit 34b30f8962787e5cb128e99ac377797cd8eab75c

Fix compile error in Windows CI job caused by 0779950768
In 6fc8d014df, pakutoma added some additional validation logic to
mb_detect_encoding. Since the implementation of mb_detect_encoding
has changed significantly between PHP 8.2 and 8.3, when merging this
change down from PHP-8.2 into master, I had to port his code over to
the new implementation in master.

However, I did this in a wrong way. In merge commit 0779950768,
the ported code modifies a function argument (to mb_guess_encoding)
which is marked 'const'. In the Windows CI job, MS VC++ rightly
flags this as a compile error.

Adjust the code to accomplish the same thing, but without destructively
modifying 'const' arguments.

ext/mbstring/mbstring.c
@@ -3001,18 +3001,6 @@ static const mbfl_encoding* mb_guess_encoding(unsigned char *in, size_t in_len, 
 		return *elist;
 	}
 
-	/* If any candidate encoding have specialized validation functions, use those first
-	 * to eliminate as many candidates as possible */
-	if (strict) {
-		for (unsigned int i = 0; i < elist_size; i++) {
-			if (elist[i]->check != NULL && !elist[i]->check(in, in_len)) {
-				elist_size--;
-				memmove(&elist[i], &elist[i+1], (elist_size - i) * sizeof(mbfl_encoding*));
-				i--;
-			}
-		}
-	}
-
 	uint32_t wchar_buf[128];
 	struct conversion_data {
 		const mbfl_encoding *enc;
@@ -3046,6 +3034,19 @@ static const mbfl_encoding* mb_guess_encoding(unsigned char *in, size_t in_len, 
 			if (in_len >= 2 && in[0] == 0xFF && in[1] == 0xFE) {
 				data[i].in_len -= 2;
 				data[i].in += 2;
+			}
+		}
+	}
+
+	/* If any candidate encodings have specialized validation functions, use them
+	 * to eliminate as many candidates as possible */
+	if (strict) {
+		for (unsigned int i = 0; i < elist_size; i++) {
+			const mbfl_encoding *enc = data[i].enc;
+			if (enc->check != NULL && !enc->check(in, in_len)) {
+				elist_size--;
+				memmove(&data[i], &data[i+1], (elist_size - i) * sizeof(struct conversion_data));
+				i--;
 			}
 		}
 	}